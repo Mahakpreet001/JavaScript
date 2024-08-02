2347f11171d3fc350d3ce3da922dcab35c1f77e3
Mahakpreet Singh <singhmahakpreet21@gmail.com>
2024-08-02 21:04:04 +0530

    conversion of datatype

---
+++ basics/03_conversionoperation.js
@@ -0,0 +1,29 @@
+let marks = 33
+let marks1 = "33"
+
+console.log(typeof marks);
+console.log(typeof marks1);
+
+let valueinNumber = Number(marks1)
+console.log(typeof valueinNumber);
+
+// "33"     =>  33
+// "33abc"  =>  NaN
+// true     =>  1; 
+// false    =>  0;
+
+let isloggedin = 1
+
+let boolenisloggedin = Boolean(isloggedin)
+console.log(boolenisloggedin);
+
+// 1 = true ; 0 = false 
+// ""     = false
+// "dfkl" = true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
+
