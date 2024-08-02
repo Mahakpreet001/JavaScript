0bd83c96eaa9e5eb753872889d1cb0ef7ab31199
Mahakpreet Singh <singhmahakpreet21@gmail.com>
2024-08-02 22:43:08 +0530

    operation

---
+++ basics/04_operation.js
@@ -0,0 +1,23 @@
+let num = 3
+let negnum = -num
+// console.log(negnum);
+
+let str1 = "Mahakpreet"
+let str2 = " Singh"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1"+2);    // 12
+// console.log(1+"2");    // 12
+// console.log("1"+2+2);  // 122
+// console.log(1+2+"2");  // 32
+
+// console.log(+true);    //1
+// console.log(+"");      //0
+
+let a,b,c;
+a = b = c = 2 + 2
+// console.log(a);     //4
+// console.log(b);     //4
+// console.log(c);     //4
+
