95aac620b314d75b56eb761c04f538af59fb580d
Mahakpreet Singh <singhmahakpreet21@gmail.com>
2024-08-02 20:42:03 +0530

    datatype

---
+++ basics/02_dataType.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+
+let name = "Mahak"
+let age = 19
+let isloggedin = true
+
+console.log(age)
+
+
+// number     =>  2^53
+// string     => " "
+// boolean    => true/false
+// null       => stand alone value
+// undefined  => 
+// symbol     => unique
+
+// objects
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);       //object
