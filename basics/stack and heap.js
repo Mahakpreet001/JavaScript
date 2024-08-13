dcdc97489722049049411bd663184648a33eb0f4
Mahakpreet Singh <singhmahakpreet21@gmail.com>
2024-08-13 13:50:40 +0530

    stack and heap

---
+++ basics/stack and heap.js
@@ -0,0 +1,18 @@
+let myname = "Mahakpreet Singh"
+
+let anothername = myname
+anothername = "Mahak"
+
+console.log(myname);
+console.log(anothername);
+
+let userone = {
+    mail: "abc@gmail.com",
+    upi: "afads@ybl"
+}
+
+let usertwo = userone
+usertwo.mail = "xyz@gmail.com"
+
+console.log(userone);
+console.log(usertwo);
