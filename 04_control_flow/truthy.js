930d30c1c8868da436af310f6c9db47e167f8f69
Mahakpreet Singh <singhmahakpreet21@gmail.com>
2024-09-10 19:45:57 +0530

    nullish operator

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,3 +1,11 @@
+const userEmail = []
+
+// if(userEmail){
+//     console.log("Got user email");
+// }else{
+//     console.log("Don't have user email");
+// }
+
 // falsy values
 
 //  false , 0 , -0 , BigInt(0n) , "" , null , undefined , NaN
@@ -5,4 +13,33 @@
 
 //  truthy values
 
-//  "0" , "false" , " " , {} , [] , function(){} ,
+//  "0" , "false" , " " , {} , [] , function(){}
+
+// if (userEmail.length === 0) {
+//     console.log("arrary is empty");
+// }
+
+const emptyobject = {}
+
+// if (Object.keys(emptyobject).length === 0) {
+//     console.log("object is empty");   
+// }
+
+//************************* Nullish Coalescing Operator (??): null/undefined ****************************************************
+
+let val1;
+// val1 = 5 ?? 10                 //-->  5
+// val1 = null ?? 10              //-->  10
+// val1 = undefined ?? 15         //-->  15
+// val1 = undefined ?? null       //-->  null
+// val1 = null ?? undefined       //-->  undefined
+// val1 = null ?? 20 ?? 10        //-->  20
+// console.log(val1);
+
+//************************************ Terniary Oprator******************
+
+// condition ? true : false
+
+const ice = 100
+
+ice >= 80 ? console.log("Greater than 80"): console.log("Less  than 80");
