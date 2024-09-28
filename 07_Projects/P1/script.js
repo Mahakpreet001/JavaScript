bd95125c8b92bc138bcf477b35ed79430c0e8bd5
Mahakpreet Singh <singhmahakpreet21@gmail.com>
2024-09-28 16:06:41 +0530

    color changer

---
+++ 07_Projects/P1/script.js
@@ -0,0 +1,25 @@
+const btn = document.querySelectorAll(".button")
+const body = document.querySelector("body")
+
+btn.forEach(function(btn){
+    console.log(btn);
+    btn.addEventListener('click',function(event){
+    console.log(event);
+    console.log(event.target);
+    if(event.target.id==='grey'){
+        body.style.backgroundColor = "grey"
+    }
+    else if(event.target.id==='white'){
+        body.style.backgroundColor = "white"
+    }
+    else if(event.target.id==='blue'){
+        body.style.backgroundColor = "blue"
+    }
+    else if(event.target.id==='yellow'){
+        body.style.backgroundColor = "yellow"
+    }
+    else if(event.target.id==='pink'){
+        body.style.backgroundColor = "pink"
+    }
+    })
+})
